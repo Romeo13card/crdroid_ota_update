304a41c81d85cfce9654959d0e8aca390749176e
Romeo13card <romeo_13card_fan@outlook.com>
2026-06-11 10:26:32 +0000

    Update autogenerated json crDroid 12.11 for fog 20260611/1781162540

--- auto_createjson_12.x.sh
+++ auto_createjson_12.x.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-maintainer="Romeo13card"                                                        # Here we get the name of maintainer
+maintainer="romeo_13card"                                                        # Here we get the name of maintainer
 path=~/crdroid                                                        # Here you will need to specify the path to the crDroid source code folder
 device=$(ls $path/out/target/product)                                                       # Here we get the name of the device based on the name of the folder
 time=$(cat $path/out/build_date.txt)                                                        # Here we get the build time
